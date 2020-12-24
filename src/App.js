3db6b111259fb38d5a705f30fdf9561225f97121
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-24 18:37:48 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/1-error-example';
+import Final from './tutorial/1-useState/final/1-error-example';
 function App() {
-  return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
-    </div>
-  )
+	return (
+		<div className="container">
+			<Setup />
+			<Final />
+		</div>
+	);
 }
 
-export default App
+export default App;
