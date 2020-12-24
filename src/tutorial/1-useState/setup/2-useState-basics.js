74d6fb8526bf44f41e981caed49c3ddfb800904a
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-24 19:52:50 +0100

    UseState - Array Example

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+// use
+// component name must be uppercase
+// must be in the function/component body
+// cannot call conditionally
 
 const UseStateBasics = () => {
 	// console.log(useState('hello world'));
 	// const value = useState(1)[0];
 	// const handler = useState(1)[1];
 	// console.log(value, handler);
-	const [text, setText] = useState('random title');
 
+	const [text, setText] = useState('random title');
 	const handleClick = () => {
 		if (text === 'random title') {
 			setText('hello world');
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Fragment } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+	const [people, setPeople] = React.useState(data);
+
+	const removeItem = (id) => {
+		let newPeople = people.filter((person) => person.id != id);
+		setPeople(newPeople);
+	};
+	return (
+		<Fragment>
+			{people.map((person) => {
+				const { id, name } = person;
+				return (
+					<div key={id} className="item">
+						<h4>{name}</h4>
+						<button onClick={() => removeItem(id)}>remove</button>
+					</div>
+				);
+			})}
+			<button className="btn" onClick={() => setPeople([])}>
+				clear items
+			</button>
+		</Fragment>
+	);
 };
 
 export default UseStateArray;
