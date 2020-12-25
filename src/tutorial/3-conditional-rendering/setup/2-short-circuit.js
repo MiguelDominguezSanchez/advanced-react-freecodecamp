5b159974cf400648e8e9ab842b80f859283a9352
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 17:55:14 +0100

    Short - Circuit Evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
+import { Fragment } from 'react';
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+	const [text, setText] = useState('');
+	const firstValue = text || 'hello world';
+	const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+	return (
+		<Fragment>
+			<h1>{text || 'john doe'}</h1>
+			{text && <h1>hello world</h1>}
+			{!text && <h1>hello world</h1>}
+		</Fragment>
+	);
 };
 
 export default ShortCircuit;
