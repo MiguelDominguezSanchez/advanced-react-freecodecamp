d7514a2c39e14f51bc5281e123702d4a066f26e8
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 10:00:16 +0100

    Functional Update Form

--- src/tutorial/1-useState/final/3-useState-array.js
+++ src/tutorial/1-useState/final/3-useState-array.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { data } from '../../../data';
 const UseStateArray = () => {
-  const [people, setPeople] = React.useState(data);
+	const [people, setPeople] = React.useState(data);
 
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
-  return (
-    <>
-      {people.map((person) => {
-        const { id, name } = person;
-        return (
-          <div key={id} className='item'>
-            <h4>{name}</h4>
-            <button onClick={() => removeItem(id)}>remove</button>
-          </div>
-        );
-      })}
-      <button className='btn' onClick={() => setPeople([])}>
-        clear items
-      </button>
-    </>
-  );
+	const removeItem = (id) => {
+		setPeople((oldPeople) => {
+			let newPeople = oldPeople.filter((person) => person.id !== id);
+			return newPeople;
+		});
+	};
+	return (
+		<>
+			{people.map((person) => {
+				const { id, name } = person;
+				return (
+					<div key={id} className="item">
+						<h4>{name}</h4>
+						<button onClick={() => removeItem(id)}>remove</button>
+					</div>
+				);
+			})}
+			<button className="btn" onClick={() => setPeople([])}>
+				clear items
+			</button>
+		</>
+	);
 };
 
 export default UseStateArray;
