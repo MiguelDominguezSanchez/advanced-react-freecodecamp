b9108d2149be6a17f5c4950e79f75d8f0c30cff7
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 09:08:39 +0100

    Simple Counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
+import { Fragment } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+	const [value, setValue] = useState(0);
+
+	const reset = () => {
+		setValue(0);
+	};
+	return (
+		<Fragment>
+			<section style={{ margin: '4rem 0' }}>
+				<h2>regular counter</h2>
+				<h1>{value}</h1>
+				<button className="btn" onClick={() => setValue(value - 1)}>
+					decerase
+				</button>
+				<button className="btn" onClick={reset}>
+					reset
+				</button>
+				<button className="btn" onClick={() => setValue(value + 1)}>
+					increase
+				</button>
+			</section>
+		</Fragment>
+	);
 };
 
 export default UseStateCounter;
