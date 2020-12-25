948d0ee8d4955ac6373c80e77bf39e9ea4da1a3d
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 10:40:05 +0100

    useEffect - Basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { Fragment } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+	const [value, setValue] = useState(0);
+	useEffect(() => {
+		console.log('call useEffect');
+		document.title = 'New Messages(${value})';
+	});
+	console.log('render component');
+	return (
+		<Fragment>
+			<h1>{value}</h1>
+			<button className="btn" onClick={() => setValue(value + 1)}>
+				click me
+			</button>
+		</Fragment>
+	);
 };
 
 export default UseEffectBasics;
