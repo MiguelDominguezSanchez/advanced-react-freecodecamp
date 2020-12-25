808f314711624efdb530c326b8998752e618d93b
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 16:28:46 +0100

    useEffect - Fetch Data

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { Fragment } from 'react';
 
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+	const [users, setUsers] = useState([]);
+
+	const getUsers = async () => {
+		const response = await fetch(url);
+		const users = await response.json();
+		setUsers(users);
+		// console.log(users);
+	};
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
+	return (
+		<Fragment>
+			<h3>github users</h3>
+			<ul className="users">
+				{users.map((user) => {
+					const { id, login, avatar_url, html_url } = user;
+					return (
+						<li key={avatar_url} alt={login}>
+							<img src={avatar_url} alt={login} />
+							<diV>
+								<h4>{login}</h4>
+								<a href={html_url}>profile</a>
+							</diV>
+						</li>
+					);
+				})}
+			</ul>
+		</Fragment>
+	);
 };
 
 export default UseEffectFetchData;
