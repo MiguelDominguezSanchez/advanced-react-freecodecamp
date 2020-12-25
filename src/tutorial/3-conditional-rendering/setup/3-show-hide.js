3f099866d73fb01ec798487661b3c3147f3652b9
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 20:16:30 +0100

    Show/Hide Component

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import { Fragment } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+	const [show, setShow] = useState(false);
+	return (
+		<Fragment>
+			<button className="btn" onClick={() => setShow(!show)}>
+				show/hide
+			</button>
+			{show && <Item />}
+		</Fragment>
+	);
+};
+
+const Item = () => {
+	const [size, setSize] = useState(window.innerWidth);
+	const checkSize = () => {
+		setSize(window.innerWidth);
+	};
+	useEffect(() => {
+		window.addEventListener('resize', checkSize);
+		return () => {
+			window.removeEventListenerr('resize', checkSize);
+		};
+	}, []);
+
+	return (
+		<div style={{ marginTop: '2rem' }}>
+			<h1>window</h1>
+			<h2>size : {size} px</h2>
+		</div>
+	);
 };
 
 export default ShowHide;
