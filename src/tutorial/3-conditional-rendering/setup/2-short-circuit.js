034313a26c5e1b459f41d74899e987cd24cb246d
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 19:16:23 +0100

    Ternary Operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -5,14 +5,24 @@ import { Fragment } from 'react';
 
 const ShortCircuit = () => {
 	const [text, setText] = useState('');
-	const firstValue = text || 'hello world';
-	const secondValue = text && 'hello world';
+	const [isError, setIsError] = useState(false);
+	// const firstValue = text || 'hello world';
+	// const secondValue = text && 'hello world';
 
 	return (
 		<Fragment>
 			<h1>{text || 'john doe'}</h1>
-			{text && <h1>hello world</h1>}
-			{!text && <h1>hello world</h1>}
+			<button className="btn" onClick={() => setIsError(!isError)}>
+				toggle error
+			</button>
+			{isError && <h1>Error...</h1>}
+			{isError ? (
+				<p>there is an error...</p>
+			) : (
+				<div>
+					<h2>there is no error</h2>
+				</div>
+			)}
 		</Fragment>
 	);
 };
