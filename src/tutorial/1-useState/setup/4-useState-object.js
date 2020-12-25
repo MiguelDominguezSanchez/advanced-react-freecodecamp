a2d95ac4d32a1062d9121903b98ee7dbee5e91a4
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-25 08:48:45 +0100

    UseState - Multiple State Values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,32 @@
 import React, { useState } from 'react';
+import { Fragment } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	const [person, setPerson] = useState({
+		name: 'peter',
+		age: 24,
+		message: 'random message',
+	});
+
+	const [name, setname] = useState('peter');
+	const [age, setAge] = useState('24');
+	const [message, setMessage] = useState('random message');
+
+	const changeMessage = () => {
+		// setPerson({ ...person, message: 'hello world' });
+		setMessage('hello world');
+	};
+
+	return (
+		<Fragment>
+			<h3>{name}</h3>
+			<h3>{age}</h3>
+			<h3>{message}</h3>
+			<button className="btn" onClick={changeMessage}>
+				change message
+			</button>
+		</Fragment>
+	);
 };
 
 export default UseStateObject;
