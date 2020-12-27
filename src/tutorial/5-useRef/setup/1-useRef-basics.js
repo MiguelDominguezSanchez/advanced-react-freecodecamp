128cc6d74531d65bb7d1a15fc97aacc7372959b3
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-27 16:23:58 +0100

    useRef

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,34 @@
 import React, { useEffect, useRef } from 'react';
+import { Fragment } from 'react';
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
-const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+const UseRefBasic = () => {
+	const refContainer = useRef(null);
+	const divContainer = useRef(null);
+	const hadleSubmit = (e) => {
+		e.preventDefault();
+		console.log(refContainer.current.value);
+		console.log(divContainer.current);
+	};
+	useEffect(() => {
+		console.log(refContainer.current);
+		refContainer.current.focus();
+	});
+	console.log(refContainer);
+	return (
+		<Fragment>
+			<form className="form" onSubmit={hadleSubmit}>
+				<div>
+					<input type="text" ref={refContainer} />
+					<button type="submit">submit</button>
+				</div>
+			</form>
+			<div ref={divContainer}>hello world</div>
+		</Fragment>
+	);
 };
 
-export default UseRefBasics;
+export default UseRefBasic;
