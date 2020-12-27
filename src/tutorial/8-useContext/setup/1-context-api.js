c41c23791cb0cecaca46cc386f3225ce73be3965
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-27 20:48:26 +0100

    Context API / - useContext

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,46 +1,48 @@
 import React, { useState, useContext } from 'react';
 import { data } from '../../../data';
+import { Fragment } from 'react';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+// two components - Provider, Consumer
+
 const ContextAPI = () => {
-  const [people, setPeople] = useState(data);
-  const removePerson = (id) => {
-    setPeople((people) => {
-      return people.filter((person) => person.id !== id);
-    });
-  };
-  return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
-  );
+	const [people, setPeople] = useState(data);
+	const removePerson = (id) => {
+		setPeople((people) => {
+			return people.filter((person) => person.id !== id);
+		});
+	};
+	return (
+		<PersonContext.Provider value={{ removePerson, people }}>
+			<h3>Context API / useContext</h3>
+			<List />
+		</PersonContext.Provider>
+	);
 };
 
-const List = ({ people, removePerson }) => {
-  return (
-    <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
-      })}
-    </>
-  );
+const List = () => {
+	const mainData = useContext(PersonContext);
+	console.log(mainData);
+	return (
+		<s>
+			{mainData.people.map((person) => {
+				return <SinglePerson key={person.id} {...person} />;
+			})}
+		</s>
+	);
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
-  return (
-    <div className='item'>
-      <h4>{name}</h4>
-      <button onClick={() => removePerson(id)}>remove</button>
-    </div>
-  );
+const SinglePerson = ({ id, name }) => {
+	const { removePerson } = useContext(PersonContext);
+
+	return (
+		<div className="item">
+			<h4>{name}</h4>
+			<button onClick={() => removePerson(id)}>remove</button>
+		</div>
+	);
 };
 
 export default ContextAPI;
